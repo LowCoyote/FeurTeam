899a0b0ff998715e672d8b312950561282b88393
LowCoyote <campmas.pierre@gmail.com>
2023-04-13 16:59:58 +0200

    add bankbuilder into bank Test

--- typescript/tests/Bank.spec.ts
+++ typescript/tests/Bank.spec.ts
@@ -2,11 +2,41 @@ import {Currency} from '../src/Currency'
 import {Bank} from '../src/Bank'
 import {Money} from "../src/Money";
 import {MissingExchangeRateError} from '../src/MissingExchangeRateError'
+class BankBuilder {
+  private currency: Currency = Currency.EUR
+  private rates: Map<Currency, number> = new Map<Currency, number>()
+
+  static aBank(): BankBuilder {
+    return new BankBuilder()
+  }
+
+  withPivotCurrency(currency: Currency): BankBuilder {
+    this.currency = currency
+    return this
+  }
+
+  withExchangeRate(currency: Currency, rate: number): BankBuilder {
+    this.rates.set(currency, rate)
+    return this
+  }
+
+  build() {
+    const bank = new Bank()
+    this.rates.forEach((rate, currency) => {
+      bank.addExchangeRate(this.currency, currency, rate)
+      bank.addExchangeRate(this.currency, this.currency, 1/rate)
+    })
+    return bank
+  }
+}
+
+const aBank = () => BankBuilder.aBank()
 
 describe('Bank', function () {
-  let bank = null
+  let bank: Bank = aBank().withPivotCurrency(Currency.EUR).withExchangeRate(Currency.USD, 1.2).build()
   beforeEach(() => {
     bank = Bank.withExchangeRate(Currency.EUR, Currency.USD, 1.2)
+    bank = BankBuilder.aBank().withPivotCurrency(Currency.EUR).withExchangeRate(Currency.USD, 1.2).build()
   })
   test('convert from eur to usd returns number', () => {
     const money:Money = new Money(Currency.EUR, 10)
